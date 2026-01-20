6682eb38d31c60a60fcae82c2e8cf389f469b83a
spharvey3 <spharvey3@gmail.com>
2026-01-20 05:05:11 +0000

    Deploying to gh-pages from @ wsteinhardt/willsteinhardt@14952cce7a8ac408bbea7c7277a4031377d11a80 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/willsteinhardt/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-received-the-koret-scholar-mentoring-award-from-ucsc-for-contributions-to-student-mentorship-star",
+          title: 'Received the Koret Scholar Mentoring Award from UCSC for contributions to student mentorship....',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-honored-to-receive-the-jason-morgan-early-career-award-from-agu-trophy",
+          title: 'Honored to receive the Jason Morgan Early Career Award from AGU! :trophy:',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/willsteinhardt/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-new-paper-accepted-in-geophysical-research-letters-degushing-single-and-linked-ruptures-in-the-laboratory-nature-newspaper",
+          title: 'New paper accepted in Geophysical Research Letters: “Degushing Single and Linked Ruptures in...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -508,14 +506,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%77%73%74%65%69%6E%68@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -530,13 +521,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
